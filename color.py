8f9fba08142128d4ecc109bd779c66a102fd1ad4
Dilip <igoldlip@gmail.com>
2023-06-17 13:27:47 +0530

    Simple python file

---
+++ color.py
@@ -0,0 +1,8 @@
+# Simple one, which asks for favorite color with pretty output
+
+name = input("sir, tell your name: ")
+favcolor = input("and what was your favorite color: ")
+print("=" * 30 + "󰅂")
+print("Hello " + name)
+print("Sent data that, " + name + "'s " + "favorite color is " + favcolor)
+print("=" * 20 + "󰅂")
