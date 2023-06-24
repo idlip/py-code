0e3df8e34257a669c53fcf0dd96100b1e7572e7f
Dilip <igoldlip@gmail.com>
2023-06-24 16:21:04 +0530

    Fixed python env with all packages

--- flake.nix
+++ flake.nix
@@ -12,10 +12,17 @@
     
   in {
     devShell.${system} = pkgs.mkShell {
-
+      
       nativeBuildInputs = [ pkgs.bashInteractive ];
       buildInputs = with pkgs; [ 
-        python
+        # python311Packages.python-lsp-server
+        # nodePackages.pyright
+        (python311.withPackages(ps: with ps; [ 
+          pylint
+          pyflakes
+          numpy
+          matplotlib matplotlib-inline
+        ]))
         # add more packages (https://search.nixos.org)
       ];
 
